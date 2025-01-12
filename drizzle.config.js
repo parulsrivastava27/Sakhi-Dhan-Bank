cd98db866c70d98586f01722f4553d9c34140a4b
Parul <parul.srivastava.2004@gmail.com>
2025-01-12 21:59:15 +0530

    initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:Wr92IqamGVXM@ep-spring-rain-a1k40xq3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:Wr92IqamGVXM@ep-spring-rain-a1k40xq3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+  },
+};
